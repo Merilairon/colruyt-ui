01593e69dff9c0618f97a3ce9244d0fcebbfa868
Jan Baetens <baetens-jan@hotmail.com>
2025-09-14 01:08:19 +0200

    Àdd basic filters

--- src/stores/filters.ts
+++ src/stores/filters.ts
@@ -6,6 +6,10 @@ const filters = persisted<
 		fromPercentage: number;
 		toPercentage: number;
 	}[]
->('filters', []);
+>('filters', [
+	{ filterName: '-100% to -50%', fromPercentage: -100, toPercentage: -50 },
+	{ filterName: '-50% to -25%', fromPercentage: -50, toPercentage: -25 },
+	{ filterName: '-25% to 0%', fromPercentage: -25, toPercentage: 0 }
+]);
 
 export default filters;
